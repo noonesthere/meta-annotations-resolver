9b487b4984b8bc8c60be811f4cc5a7112a1537a5
NoOnesThere <d.zlobenets@gmail.com>
2025-09-10 22:18:48 +0300

    draft version

--- src/main/kotlin/com/github/noonesthere/metaannotationsresolver/discoverer/MetaAnnotationStereotypeDiscoverer.kt
+++ src/main/kotlin/com/github/noonesthere/metaannotationsresolver/discoverer/MetaAnnotationStereotypeDiscoverer.kt
@@ -1,64 +1,57 @@
 package com.github.noonesthere.metaannotationsresolver.discoverer
 
 import com.intellij.openapi.module.Module
-import com.intellij.psi.PsiAnnotation
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.search.PsiShortNamesCache
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiManager
+import com.intellij.psi.search.FilenameIndex
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.spring.model.CommonSpringBean
-import com.intellij.spring.model.jam.contexts.CustomContextJavaBean
-import com.intellij.spring.model.custom.CustomModuleComponentsDiscoverer
+import com.intellij.spring.model.SpringImplicitBean
+import com.intellij.spring.model.SpringImplicitBeansProviderBase
+import org.jetbrains.annotations.NotNull
 
-class MetaAnnotationStereotypeDiscoverer : CustomModuleComponentsDiscoverer() {
-
-    private val springStereotypeFqns = setOf(
+class MetaAnnotationStereotypeDiscoverer : SpringImplicitBeansProviderBase() {
+    private val stereotypes = setOf(
         "org.springframework.stereotype.Component",
         "org.springframework.stereotype.Service",
         "org.springframework.stereotype.Repository",
         "org.springframework.stereotype.Controller",
-        "org.springframework.stereotype.Configuration"
+        "org.springframework.stereotype.Configuration",
+        "org.springframework.web.bind.annotation.RestController"
     )
 
-    override fun getCustomComponents(p0: Module): Collection<CommonSpringBean?> {
-        TODO("Not yet implemented")
+    override fun getImplicitBeans(@NotNull module: Module): Collection<CommonSpringBean> {
+        val project = module.project
+        val javaFiles = FilenameIndex.getAllFilesByExt(project, "java", module.moduleProductionSourceScope)
+
+        val beans = mutableListOf<CommonSpringBean>()
+
+        for (vf in javaFiles) {
+            val psiFile = PsiManager.getInstance(project).findFile(vf) ?: continue
+            val psiClass = PsiTreeUtil.getChildOfType(psiFile.originalElement, PsiClass::class.java) ?: continue
+
+            // skip interfaces, enums, annotations
+            if (psiClass.isInterface || psiClass.isEnum || psiClass.isAnnotationType) continue
+
+            if (hasMetaStereotype(psiClass)) {
+                val beanName = psiClass.name?.replaceFirstChar { it.lowercase() } ?: continue
+                beans.add(SpringImplicitBean(getProviderName(), psiClass, beanName))
+            }
+        }
+        return beans
     }
 
-    override fun getProviderName(): String {
-        TODO("Not yet implemented")
+    private fun hasMetaStereotype(psiClass: PsiClass): Boolean {
+        for (annotation in psiClass.annotations) {
+            val annType = annotation.resolveAnnotationType() ?: continue
+            for (meta in annType.annotations) {
+                if (meta.qualifiedName in stereotypes) {
+                    return true
+                }
+            }
+        }
+        return false
     }
 
-    override fun getDependencies(p0: Module): Array<out Any?>? {
-        TODO("Not yet implemented")
-    }
-
-//    override fun getCustomComponents(module: Module): Collection<CommonSpringBean> {
-//        val project = module.project
-//        val scope = GlobalSearchScope.moduleScope(module)
-//        val cache = PsiShortNamesCache.getInstance(project)
-//        val beans = mutableListOf<CommonSpringBean>()
-//
-//        for (className in cache.allClassNames) {
-//            val classes = cache.getClassesByName(className, scope)
-//            for (psiClass in classes) {
-//                val hasMetaAnnotation = psiClass.annotations.any { isMetaAnnotatedWithSpringStereotype(it) }
-//                if (hasMetaAnnotation) {
-//                    // Create a bean that IDE can navigate
-//                    val bean = CustomContextJavaBean(psiClass, this)
-//                    beans.add(bean)
-//                }
-//            }
-//        }
-//
-//        return beans
-//    }
-//
-//    override fun getProviderName(): String = "MetaAnnotationStereotypeDiscoverer"
-//
-//    override fun getDependencies(module: Module): Array<Any>? = null
-//
-//    private fun isMetaAnnotatedWithSpringStereotype(annotation: PsiAnnotation): Boolean {
-//        val resolvedClass = annotation.resolveAnnotationType() ?: return false
-//        return resolvedClass.annotations.any { meta ->
-//            springStereotypeFqns.contains(meta.qualifiedName)
-//        }
-//    }
+    override fun getProviderName(): String = "MetaAnnotation Bean Provider"
 }
