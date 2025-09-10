cb5ff85352555921010c1dc8886dde7c83249aae
GitHub Action <action@github.com>
2025-09-10 08:36:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/noonesthere/metaannotationsresolver/services/MyProjectService.kt
+++ src/main/kotlin/com/github/noonesthere/metaannotationsresolver/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.noonesthere.metaannotationsresolver.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.noonesthere.metaannotationsresolver.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/noonesthere/metaannotationsresolver/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/noonesthere/metaannotationsresolver/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.noonesthere.metaannotationsresolver.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
